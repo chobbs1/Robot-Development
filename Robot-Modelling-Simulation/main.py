6a875413874290d5e34bad1d10a88d1ae71a22dc
Unknown <cm.hobbs94@gmail.com>
2020-06-09 18:30:58 +1000

    Commit to remove stashed

--- Robot-Modelling-Simulation/main.py
+++ Robot-Modelling-Simulation/main.py
@@ -4,6 +4,17 @@ from RobotModel import Robot
 from Visualisation import VisualisationTools
 from matplotlib import pyplot as plt
 import matplotlib.animation as animation
+from datetime import date,time
+import time as timeModule
+import csv
+
+def getlogTitle():
+    today = date.today()
+    current_date = today.strftime("_%b_%d_%Y")
+    t = timeModule.localtime()
+    current_time = timeModule.strftime("%H-%M-%S", t)
+    log_title = "Data_Log_" + current_time + current_date
+    return log_title
 
 # define simulation environment conditions
 duration = 10
@@ -22,14 +33,22 @@ print("---Begin Simulation---")
 initial_conditions = [x,x_dot,y,y_dot,theta]
 mRobot = Robot(initial_conditions)
 mPlotter = VisualisationTools()
+file = open("Logs/"+getlogTitle()+".csv", 'w')
+dataLogger = csv.writer(file)
 
 
-# for i in range(int(duration/dt)):
-#     time += dt
-t = np.arange(0,duration,dt)
-y = np.sin(2*np.pi*t)*np.exp(-t)
-x = 5*np.ones(len(y))
 
+for i in range(int(duration/dt)):
+    time += dt
+    x = i
+
+    state_vector = [x,x_dot,y,y_dot,theta]
+
+    dataLogger.writerow(state_vector)
+
+
+###---------------Log Output----------------###
+dataLogger.writerow("x,x_dot,y,y_dot,theta")
 
 ###------------visualise output-------------###
 # mPlotter.animateRobot(x,y,t)
@@ -37,5 +56,5 @@ x = 5*np.ones(len(y))
 
 
 
-
+file.close()
 print("---Simulation Complete---")
